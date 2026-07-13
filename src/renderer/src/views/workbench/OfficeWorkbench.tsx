17ec8c22d64bdd3a233933b90740196f0ccd0265
kaieye <623917753@qq.com>
2026-07-13 14:23:32 +0800

    Remove unused Marvis workbench assets
    Includes the current workbench stage shift adjustment.

--- src/renderer/src/views/workbench/OfficeWorkbench.tsx
+++ src/renderer/src/views/workbench/OfficeWorkbench.tsx
@@ -60,7 +60,7 @@ type WorkbenchSeatState = {
 
 const officeWidth = 64 * 17
 const officeHeight = 64 * 14
-const stageShift = { x: 40, y: -20 }
+const stageShift = { x: 0, y: -20 }
 const canvasOutputScale = 2
 const officeScaleBoost = 0.78
 const compactScaleBoost = 0.5
